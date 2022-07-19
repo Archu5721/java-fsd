e816108d7d68289cbf7e9495a929996e9c1e1a0d
Archatha <archatha18022@cse.ssn.edu.in>
2022-07-19 17:45:40 +0530

    changes

---
+++ Practice-Project1/Arrays.java
@@ -0,0 +1,15 @@
+
+public class Arrays {
+	public static void main(String[] args) {
+		int arr[]= {5,3,4,2,10};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		String name[] = {"abc","efg","hij","lmnop"};
+		for(int j=0;j<name.length;j++) {
+			System.out.println(name[j]);
+		}
+		int array[][]= {{2,3},{1,2,3}};
+		System.out.println(array.length);
+	}
+}
