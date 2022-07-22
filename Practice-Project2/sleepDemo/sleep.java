971d511fef297b3289ff761b0fa7cf20f821e347
Archatha <archatha18022@cse.ssn.edu.in>
2022-07-22 15:16:57 +0530

    P2

--- Practice-Project2/sleepDemo/sleep.java
+++ Practice-Project2/sleepDemo/sleep.java
@@ -6,12 +6,18 @@ public class sleep extends Thread{
 		sl.setName("Thread 1");
 		sl.start();
 		for(int i=1;i<=5;i++) {
-			System.out.print(i +":Aria ");
+			System.out.println(i +":Aria ");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	public void run() {
 		for(int j=1;j<=5;j++) {
-			System.out.print(j + ":Hello!! ");
+			System.out.println(j + ":Hello!! ");
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
