79b44568f7e67750917baf228fecd59dd033a463
Archatha <archatha18022@cse.ssn.edu.in>
2022-07-28 14:29:52 +0530

    P3

---
+++ Practice-Project3/Project_3/Multipication.java
@@ -0,0 +1,28 @@
+package Project_3;
+
+public class Multipication {
+	public static void main(String[] args) {
+		int m1[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+		int m2[][] = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		int result[][] = new int[2][2];
+		int sum = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 3; k++) {
+					sum += m1[i][k] * m2[k][j];
+				}
+				result[i][j] = sum;
+				sum = 0;
+			}
+
+		}
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+
+}
